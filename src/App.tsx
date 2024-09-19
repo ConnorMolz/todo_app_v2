df3fe1cbae293d63f22c6ce10d875ca47d3bdc59
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-09-19 14:49:02 +0200

    Change Clients to the central clients from the lib folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
 import { useState, useEffect } from 'react'
-import {createClient, Session} from '@supabase/supabase-js'
+import { Session } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import Home from "./Pages/Home.tsx";
-
-const supabase = createClient('https://jwxjbmsorwybofnzlvrh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3eGpibXNvcnd5Ym9mbnpsdnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwMDM2NTIsImV4cCI6MjAzOTU3OTY1Mn0.3t4P6liovQTg5p1eg8KKzLFYyswzN8gizi9QBEl8NQw')
+import { supabase } from "./lib/supabase.ts";
 
 export default function App() {
     const [session, setSession] = useState<Session | null>(null)
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import { createClient, Session } from "@supabase/supabase-js";
+import { Session } from "@supabase/supabase-js";
 import NavBar from "../Components/NavBar.tsx";
 import { Link } from 'react-router-dom';
 import { invoke } from "@tauri-apps/api/core";
+import { supabase } from "../lib/supabase.ts";
 
-const supabase = createClient('https://jwxjbmsorwybofnzlvrh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3eGpibXNvcnd5Ym9mbnpsdnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwMDM2NTIsImV4cCI6MjAzOTU3OTY1Mn0.3t4P6liovQTg5p1eg8KKzLFYyswzN8gizi9QBEl8NQw')
 
 const Home = () =>{
     const [ session, setSession ] = useState<Session | null>(null);
