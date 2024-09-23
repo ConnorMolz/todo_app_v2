b1be275aecdb1f36a3a122d2757ea434f467ac46
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-09-23 12:15:18 +0200

    Update and comment LogInHeader Component

--- src/Components/LogInHeader.tsx
+++ src/Components/LogInHeader.tsx
@@ -1,8 +1,14 @@
+// Small Header for the login page
+// The Header has no functions it's only for the optic
 const LogInHeader = () => {
     return(
         <div>
             <div className="navbar bg-base-200">
-                <a className="btn btn-ghost text-xl">Todo App</a>
+                <div className="navbar-start"></div>
+                <div className="navbar-center">
+                    <a className="btn btn-ghost text-xl">Todo App</a>
+                </div>
+                <div className="navbar-end"></div>
             </div>
         </div>
     )
