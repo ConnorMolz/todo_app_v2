8514ca228aa692b1faf88a5bf3f72a298a371221
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-09-23 12:33:07 +0200

    Commenting Home Page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,36 +1,49 @@
 import { useEffect, useState } from "react";
 import NavBar from "../Components/NavBar.tsx";
-import { Link } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import {pocket_base} from "../lib/pocket_base.ts";
 import {AuthModel, RecordModel} from "pocketbase";
 
 
 const Home = () =>{
 
+    // Page Variables
     const [ session, setSession ] = useState<AuthModel | null>(null);
     const [ todos, setTodos ] = useState<RecordModel | []>([])
+    // Navitgator for send back if the session is invalid
+    const navigate = useNavigate();
 
+    // Check session
     useEffect(()=>{
 
         if(pocket_base.authStore.isValid){
             setSession(pocket_base.authStore.model);
         }
+        else{
+            navigate("/");
+        }
     },[]);
 
+    // On setSession pull the todos from pocket Base
     useEffect(() => {
         if (session) getTodos().then();
     },[session]);
 
+    // Function which pull the todos
     async function getTodos() {
+        // If the session is not set at this moment the app wait 1 Second and try it again
         if(!session)setTimeout(() => getTodos(), 1000);
         if(!session) return;
+        // The query filter apply the filter at the backend, that the user only get his own todos
         const queryFilter = "user_id = \"" + session.id + "\" && done = false";
+        // Pull the data
         const data = await pocket_base.collection('todos').getFullList(
             {
                 filter: queryFilter,
             }
         );
         //@ts-ignore   TODO: update data type later in useState
+        // Set page data
         setTodos(data);
 
     }
