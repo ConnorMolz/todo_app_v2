9a2a3e0461466c70fbc849275a3a0724a9245365
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-10-10 07:41:54 +0200

    Fix optic error and add the corporate Theme

--- src/Components/Settings/Appearence/ThemeChooser.tsx
+++ src/Components/Settings/Appearence/ThemeChooser.tsx
@@ -3,17 +3,20 @@ import {themeChange} from "theme-change";
 const ThemeChooser = () => {
 
     function changeTheme() {
-        themeChange(true);
-        setTimeout(() => {themeChange(false)}, 2000);
+        // Function to enable the theme change
+        themeChange(false);
+        // Disable after 2 seconds
+        setTimeout(() => {themeChange(true)}, 2000);
     }
 
     return (
         <div>
             <h1>Theme Chooser</h1>
-            <select data-choose-theme onChange={changeTheme}>
+            <select data-choose-theme onChange={changeTheme} className="select select-bordered w-full max-w-xs">
                 <option value="">Default</option>
                 <option value="dark">Dark</option>
                 <option value="light">Light</option>
+                <option value="corporate">Corporate</option>
             </select>
         </div>
     )
