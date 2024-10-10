2beb66f0d38f1abc58249b8b0d5104de59e0658c
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-10-10 07:35:21 +0200

    Implement ThemeChooser in Settings

--- src/Components/Settings/Appearence/ThemeChooser.tsx
+++ src/Components/Settings/Appearence/ThemeChooser.tsx
@@ -1,7 +1,20 @@
+import {themeChange} from "theme-change";
+
 const ThemeChooser = () => {
+
+    function changeTheme() {
+        themeChange(true);
+        setTimeout(() => {themeChange(false)}, 2000);
+    }
+
     return (
         <div>
             <h1>Theme Chooser</h1>
+            <select data-choose-theme onChange={changeTheme}>
+                <option value="">Default</option>
+                <option value="dark">Dark</option>
+                <option value="light">Light</option>
+            </select>
         </div>
     )
 }
