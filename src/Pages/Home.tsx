824b1abe56a2a2afba7aed07be3022630f2e652e
ConnorMolz <114417919+ConnorMolz@users.noreply.github.com>
2024-10-15 14:46:25 +0200

    Put the ownership filter into a new dropdown and the dropdown is now marked with ... and react on hover

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -69,22 +69,30 @@ const Home = () => {
     return (
         <div className="bg-base-100">
             <NavBar/>
-            <div className="justify-center flex pt-5">
-                <details className="dropdown">
-                    <summary className="btn btn-neutral m-1">Toggle Filter</summary>
-                    <ul className="menu dropdown-content bg-base-200 rounded-box z-[1] w-52 p-2">
-                        <li>
-                            <div className="flex justify-center">
-                                <button
-                                    className="hover"
-                                    onClick={() => setFilterForOwnTodos(!filterForOwnTodos)}
-                                >
-                                    {filterForOwnTodos ? 'Show All Todos' : 'Show My Todos'}
-                                </button>
-                            </div>
-                        </li>
-                    </ul>
-                </details>
+            <div className="dropdown dropdown-hover dropdown-end justify-end flex">
+                <div tabIndex={0} role="button" className="btn btn-ghost m-1">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block h-5 w-5 stroke-current">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+                    </svg>
+                </div>
+                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li>
+                        <button
+                            className="hover"
+                            onClick={() => setFilterForOwnTodos(!filterForOwnTodos)}
+                        >
+                            {filterForOwnTodos ? 'Show All Todos' : 'Show Only My Todos'}
+                        </button>
+                    </li>
+                </ul>
             </div>
             <div className="flex-1 justify-center">
                 {!loading &&
